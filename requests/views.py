2d6e03d73f666dfa3f989952a3fe88823e62b28e
tuiky <damir_ne_@mail.ru>
2014-08-08 18:50:36 +0300

    ticket 3: now show_requests view shows only first 10 http requests that are stored by middleware

--- requests/views.py
+++ requests/views.py
@@ -4,4 +4,4 @@ from .models import Request
 
 # Create your views here.
 def show_requests(request):
- 	return render(request, 'requests.html', {'requests': Request.objects.all()})
+ 	return render(request, 'requests.html', {'requests': Request.objects.all()[:10]})
