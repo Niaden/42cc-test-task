e3941ab3b9dd2df052504b886ffeadfdd2d40ab5
tuiky <damir_ne_@mail.ru>
2014-08-08 19:05:41 +0300

    ticket 3: using django.utils.timezone.now instead of datetime.datetime.now

--- requests/middleware.py
+++ requests/middleware.py
@@ -1,12 +1,12 @@
 from .models import Request
 from django.http.request import HttpRequest
-import datetime
+from django.utils import timezone
 
 class WebRequestMiddleware(object):
 	def process_request(self, request):
 		req_path =  request.get_full_path()
 		req_method = request.method
-		req_datetime = datetime.datetime.now()
+		req_datetime = timezone.now()
 		request_ob = Request(request_date=req_datetime, request_method=req_method, request_path=req_path)
 		request_ob.save()
 		return None
